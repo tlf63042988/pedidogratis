7566799ba99e1b0831a14d51b9ae86168bda137c
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-17 23:37:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
-var express = require('express'),
-    app = express();
-
+const express = require('express');
 const path = require('path');
-
-
+const app = express();
 app.use(express.static(__dirname + 'www'));
-
+// Link index.html of build folder with router.
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/www/index.html'));
+    res.sendFile(path.join(__dirname + 'www/index.html'));
 });
-
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-
-// API Routes
-// app.get('/adl', routeHandler);
-
-app.set('port', process.env.PORT || 5000);
-
-
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
-
+app.listen(process.env.PORT || 5000);
