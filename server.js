bcd6f1a257864e6c45012676cf2ef8e3c9c02130
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-18 00:26:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static('www'));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("www"));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../www/index.html"));
   });
 }
