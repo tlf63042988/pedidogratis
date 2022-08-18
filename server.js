892fcf0103d7a65a2860cd815ca83d75d5d8dbca
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-17 22:54:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+var express = require('express'),
+    app = express();
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    'extended': 'true'
-}));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.static('www'));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Orgin', '*');
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested_With, COntent-type, Accept');
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
-app.use(express.static('www'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/www/index.html'));
-});
+// API Routes
+// app.get('/blah', routeHandler);
+
 app.set('port', process.env.PORT || 5000);
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/www/index.html'));
 });
