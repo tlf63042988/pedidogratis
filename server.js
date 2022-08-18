cd30d71b4e61fdaff373814397032437287d2736
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-17 23:45:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
-// Link index.html of build folder with router.
-app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/<FOLDER_NAME>/index.html'));
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Orgin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested_With, COntent-type, Accept');
+    next();
 });
-app.listen(process.env.PORT || 5000);
+
+app.use(express.static('www'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/www/index.html'));
+});
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
