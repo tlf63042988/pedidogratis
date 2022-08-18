3296c6a847ef392e2d747eb886351d53eec20f7b
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-17 23:28:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express'),
     app = express();
 
+const path = require('path');
+
+
 app.use(express.static('www'));
+
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/www/index.html'));
+});
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -15,8 +22,10 @@ app.all('*', function(req, res, next) {
 
 app.set('port', process.env.PORT || 5000);
 
+
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
-const path = require("path");
+
