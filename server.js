8d77eb3497550c1c9593b786bc25454f55a9c928
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-17 23:54:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use(express.static('www'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/www/index.html'));
 });
+
+app.get('/*', (req, res) => res.send('/www/index.html'));
+
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
