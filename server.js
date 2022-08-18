0a470965ff3e62f3a764d5053914b47eb06f0669
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-18 01:46:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   //app.get("/*", (req, res) => {
  // res.sendFile(path.join(__dirname, "./www/index.html"));
  // });
- app.get('/*', (req, res) => res.send('www<a href="/">volver</a>'));
+ app.get('/*', (req, res) => res.send('<br><br>Re Ingresar<a href="/">volver</a>'));
 //app.get('/*', (req, res) => res.send(volver <a href='/'>algo</a>));
 }
 
