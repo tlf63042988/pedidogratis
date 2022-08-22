fe290af87488c5eb1c6e00db1187db15b423cc73
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-22 10:13:15 -0500

    Update paises.service.ts

--- src/app/service/paises.service.ts
+++ src/app/service/paises.service.ts
@@ -9,11 +9,11 @@ export class PaisesService {
   countryData =  [
     
     {"nombre":"Av Huarochiri Av metropolitana","enlace":"assets/mapasretiro/av-huarochiri-av-metropolitana.png"},
-   {"nombre":"Bembos La Fontana":assets/mapasretiro/bembos-la-fontana.png"},
+   {"nombre":"Bembos La Fontana","enlace":assets/mapasretiro/bembos-la-fontana.png"},
    {"nombre":"La Lucha Larco","enlace":"assets/mapasretiro/la-lucha-larco.png/mapasretiro/bembos-la-fontana.png"},
    {"nombre":"Las Canastas La Molina","enlace":"assets/mapasretiro/las-canastas-la-molina.png"},
    {"nombre":"Mall Aventura de Santa Anita #1","enlace":"assets/mapasretiro/mall-aventura-de-santa-anita-1.png"},
-   {"nombre":"Mall Aventura de Santa Anita #2":"assets/mapasretiro/mall-aventura-de-santa-anita-2.png"},
+   {"nombre":"Mall Aventura de Santa Anita #2","enlace":"assets/mapasretiro/mall-aventura-de-santa-anita-2.png"},
    {"nombre":"Metro La Molina","enlace":"assets/mapasretiro/metro-la-molina.png"},
    {"nombre":"Municipalidad de Santa Anita","enlace":"assets/mapasretiro/municipalidad-de-santa-anita.png"},
    {"nombre":"Pardos Chicken Javier Prado C/ Ingenieros","enlace":"assets/mapasretiro/pardos-chicken-javier-prado-ingenieros.png"},
