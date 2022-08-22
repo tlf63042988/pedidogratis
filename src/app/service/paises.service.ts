cc21e1c61e6f698bbd3161d68856e5394cff52a3
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-22 10:06:19 -0500

    Subida de mapas al retirar

--- src/app/service/paises.service.ts
+++ src/app/service/paises.service.ts
@@ -8,19 +8,19 @@ export class PaisesService {
 
   countryData =  [
     
-    {"nombre":"con iconos","enlace":"assets/mapasretiro/av-huarochiri-av-metropolitana.png"},
-   {"nombre":"Mapa retiro 2","enlace":assets/mapasretiro/bembos-la-fontana.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/la-lucha-larco.png/mapasretiro/bembos-la-fontana.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/las-canastas-la-molina.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/mall-aventura-de-santa-anita-1.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/mall-aventura-de-santa-anita-2.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/metro-la-molina.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/municipalidad-de-santa-anita.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/pardos-chicken-javier-prado-ingenieros.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/prueba-con-iconos.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/real-plaza-puruchucu-1.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/roll-star-sushi-bar-1.png"},
-   {"nombre":"Mapa retiro 2","enlace":"assets/mapasretiro/wong-ate.png"}
+    {"nombre":"Av Huarochiri Av metropolitana","enlace":"assets/mapasretiro/av-huarochiri-av-metropolitana.png"},
+   {"nombre":"Bembos La Fontana":assets/mapasretiro/bembos-la-fontana.png"},
+   {"nombre":"La Lucha Larco","enlace":"assets/mapasretiro/la-lucha-larco.png/mapasretiro/bembos-la-fontana.png"},
+   {"nombre":"Las Canastas La Molina","enlace":"assets/mapasretiro/las-canastas-la-molina.png"},
+   {"nombre":"Mall Aventura de Santa Anita #1","enlace":"assets/mapasretiro/mall-aventura-de-santa-anita-1.png"},
+   {"nombre":"Mall Aventura de Santa Anita #2":"assets/mapasretiro/mall-aventura-de-santa-anita-2.png"},
+   {"nombre":"Metro La Molina","enlace":"assets/mapasretiro/metro-la-molina.png"},
+   {"nombre":"Municipalidad de Santa Anita","enlace":"assets/mapasretiro/municipalidad-de-santa-anita.png"},
+   {"nombre":"Pardos Chicken Javier Prado C/ Ingenieros","enlace":"assets/mapasretiro/pardos-chicken-javier-prado-ingenieros.png"},
+   {"nombre":"Mapa en blanco","enlace":"assets/mapasretiro/prueba-con-iconos.png"},
+   {"nombre":"Real Plaza Puruchucu #1","enlace":"assets/mapasretiro/real-plaza-puruchucu-1.png"},
+   {"nombre":"Roll Star Suchi Bar #1":"assets/mapasretiro/roll-star-sushi-bar-1.png"},
+   {"nombre":"Wong Ate","enlace":"assets/mapasretiro/wong-ate.png"}
     ]
 
 
