13c2ce17f9d25df0fe4452f493b105e69b3b238c
tlf63042988 <74887908+tlf63042988@users.noreply.github.com>
2022-08-26 16:49:38 -0500

    Update paises.service.ts

--- src/app/service/paises.service.ts
+++ src/app/service/paises.service.ts
@@ -14,7 +14,7 @@ export class PaisesService {
   {"nombre":"Pikadito Chicken","enlace":"assets/mapasretiro/pikadito-chicken.png"},
   {"nombre":"Papa John's Camacho","enlace":"assets/mapasretiro/papa-johns-camacho.png"},
   {"nombre":"Papa John's Repsol","enlace":"assets/mapasretiro/papa-johns-repsol.png"},
-  {"nombre":"Little Caesar Pizza # La Molina","enlace":"assets/mapasretiro/little-cesar-pizza-la-molina.png"},
+  {"nombre":"Little Caesars' Pizza - La Molina","enlace":"assets/mapasretiro/little-cesar-pizza-la-molina.png"},
   {"nombre":"El Hornero","enlace":"assets/mapasretiro/el-hornero.png"},
   {"nombre":"Av Huarochiri Av metropolitana","enlace":"assets/mapasretiro/av-huarochiri-av-metropolitana.png"},
   {"nombre":"Bembos - La Fontana","enlace":"assets/mapasretiro/bembos-la-fontana.png"},
